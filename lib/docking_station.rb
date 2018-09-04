9385904c7e85ead2af239dd79e5aebd667ad03c1
fbl11 <freyab_london@yahoo.co.uk>
2018-09-04 17:48:01 +0100

    first user story step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,13 @@
 require_relative './bike'
 
 class DockingStation
+	attr_reader :bikes
+	def initialize
+		@bikes = []
+	end
+	def dock_bike(bike)
+		@bikes.push(bike)
+	end
 	def release_bike
 		Bike.new
 	end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,11 @@ describe DockingStation do
 	it 'gets a bike and expects bike to be working' do
 		expect(subject.release_bike).to be_working
 	end
+	it { should respond_to(:dock_bike) }
+	it 'adds bike to docking station' do
+		bike = Bike.new
+		subject.dock_bike(bike)
+		expect(subject.bikes.include?(bike)).to eq(true)
+	end
 
 end
